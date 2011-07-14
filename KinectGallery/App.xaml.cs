897a2bcff85db6257ca47303d4a9051face450b8
Davis Marques <dmarq.ezz@gmail.com>
2011-07-14 17:50:20 -0400

    First commit

---
+++ KinectGallery/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace Ryerson.KinectGallery
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        /// <summary>
+        /// Application_Startup event handler.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // KinectWindow kw = new KinectWindow();
+            // kw.Visibility = Visibility.Visible;
+
+            //MouseWindow mw = new MouseWindow();
+            //mw.Visibility = Visibility.Visible;
+
+            //MainWindow mw2 = new MainWindow();
+            //mw2.Visibility = Visibility.Visible;
+
+            MenuWindow mw3 = new MenuWindow();
+            mw3.Visibility = Visibility.Visible;
+        }
+
+    }
+}
---
+++ KinectGallery/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ryerson.KinectGallery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region fields
+        #endregion fields
+
+        #region constructors
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #endregion constructors
+
+        #region methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Border_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (e.OriginalSource is Border)
+            {
+                Border b = (Border) e.OriginalSource;
+                b.Opacity = 0.8;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Border_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (e.OriginalSource is Border)
+            {
+                Border b = (Border)e.OriginalSource;
+                b.Opacity = 1;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Quad1_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Quad1 mouse up");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Quad2_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Quad2 mouse up");
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Quad3_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Quad3 mouse up");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Quad4_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine("Quad4 mouse up");
+        }
+
+        #endregion methods
+
+    }
+}
